2c3ee6015dbaa398458a62d70036d738165a6ecd
Abhishek Jain <abhishek.j0407@gmail.com>
2022-03-25 00:46:23 +0530

    Form section completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,48 +1,92 @@
 import { useEffect, useState } from "react";
 
 const Form = () => {
+  const [feedback, setFeedback] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    hostXp: "",
+    beverageXp: "",
+    cleanXp: "",
+    diningXp: "",
+  });
 
-  const [feedback, setFeedback] = useState(
-    {
-      name: "",
-      email: "",
-      phone: "",
-      hostXp: "",
-      beverageXp: "",
-      cleanXp: "",
-      diningXp: ""
-    });
-  
-  const [feedbacks, setFeedbacks] = useState([])
+  const [feedbacks, setFeedbacks] = useState([]);
+
+  const [errors, setErrors] = useState({});
+
+  const [isSubmit, setIsSubmit] = useState(false);
 
   //Get Feedbacks collection from local storage, if it exists
   useEffect(() => {
-    const localFeedbacks = localStorage.getItem("feedbacks")
+    const localFeedbacks = localStorage.getItem("feedbacks");
     if (localFeedbacks) {
-      setFeedbacks(JSON.parse(localFeedbacks))
+      setFeedbacks(JSON.parse(localFeedbacks));
     }
-  },[])
+  }, []);
 
   //On submit, update collection with the new feedback
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setFeedbacks([...feedbacks, feedback])
-  }
+    e.preventDefault();
+    setErrors(validate(feedback));
+    setIsSubmit(true);
+    // if (isSubmit) {
+    //   setFeedbacks([...feedbacks, feedback])
+    // }
+  };
 
-  
+  useEffect(() => {
+    // console.log(errors);
+    if (Object.keys(errors).length === 0 && isSubmit) {
+      setFeedbacks([...feedbacks, feedback]);
+      var form = document.getElementById("main-form");
+      form.reset();
+      setFeedback({})
+    }
+  }, [errors]);
+
   //Update feedback fields on change
   function handleChange(e) {
-    setFeedback({ ...feedback, [e.target.name]: e.target.value })
+    setFeedback({ ...feedback, [e.target.name]: e.target.value });
   }
+
+  //Validate radio inputs
+  const validate = (values) => {
+    const formErrors = {};
+
+    if (!values.hostXp) {
+      formErrors.hostXp = "* Please select an option.";
+    }
+
+    if (!values.beverageXp) {
+      formErrors.beverageXp = "* Please select an option.";
+    }
+
+    if (!values.diningXp) {
+      formErrors.diningXp = "* Please select an option.";
+    }
+
+    if (!values.cleanXp) {
+      formErrors.cleanXp = "* Please select an option.";
+    }
+
+    return formErrors;
+  };
 
   //On change of collection(i.e. on submit), save collection to local storage
   useEffect(() => {
-    localStorage.setItem("feedbacks", JSON.stringify(feedbacks))
-  },[feedbacks])
+    if (feedbacks.length !== 0)
+      localStorage.setItem("feedbacks", JSON.stringify(feedbacks));
+  }, [feedbacks]);
 
   return (
     <div className="form-container">
-      <form className="form-wrapper" autoComplete="off" onSubmit={(e) => handleSubmit(e)}>
+      <form
+        className="form-wrapper"
+        id="main-form"
+        autoComplete="off"
+        onSubmit={(e) => handleSubmit(e)}
+      >
         <div className="form-column form-left">
           <div className="form-intro">
             <h2>Aromatic Bar</h2>
@@ -67,7 +111,7 @@ const Form = () => {
                 <span className="label-text">
                   Name
                   <span className="error-indicator active-error-indicator">
-                    *
+                    {errors.name}
                   </span>
                 </span>
               </label>
@@ -82,6 +126,7 @@ const Form = () => {
                 onChange={(e) => handleChange(e)}
                 autoComplete="off"
                 required
+                pattern="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
               />
               <label htmlFor="email" className="label-name">
                 <span className="label-text">
@@ -99,6 +144,7 @@ const Form = () => {
                 onChange={(e) => handleChange(e)}
                 autoComplete="off"
                 required
+                pattern="\+[0-9]{2,3}-[0-9]{10}"
               />
               <label htmlFor="phone" className="label-name">
                 <span className="label-text">
@@ -112,13 +158,18 @@ const Form = () => {
         <div className="form-column form-right">
           <div className="radio-field">
             <h4>
-              1.Please rate the quality of the service you received from your
+              1 . Please rate the quality of the service you received from your
               host.
             </h4>
             <div className="buttons">
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="hostXp" value="Excellent" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="hostXp"
+                    value="Excellent"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Excellent</span>
@@ -126,7 +177,12 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="hostXp" value="Good" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="hostXp"
+                    value="Good"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Good</span>
@@ -134,7 +190,12 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="hostXp" value="Fair" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="hostXp"
+                    value="Fair"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Fair</span>
@@ -142,20 +203,31 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="hostXp" value="Bad" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="hostXp"
+                    value="Bad"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Bad</span>
               </div>
             </div>
+            <div className="radio-error">{errors.hostXp}</div>
           </div>
 
           <div className="radio-field">
-            <h4>2.Please rate the quality of your beverage.</h4>
+            <h4>2 . Please rate the quality of your beverage.</h4>
             <div className="buttons">
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="beverageXp" value="Excellent" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="beverageXp"
+                    value="Excellent"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Excellent</span>
@@ -163,7 +235,12 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="beverageXp" value="Good" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="beverageXp"
+                    value="Good"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Good</span>
@@ -171,7 +248,12 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="beverageXp" value="Fair" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="beverageXp"
+                    value="Fair"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Fair</span>
@@ -179,20 +261,31 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="beverageXp" value="Bad" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="beverageXp"
+                    value="Bad"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Bad</span>
               </div>
             </div>
+            <div className="radio-error">{errors.beverageXp}</div>
           </div>
 
           <div className="radio-field">
-            <h4>3. Was our restaurant clean?</h4>
+            <h4>3 . Was our restaurant clean?</h4>
             <div className="buttons">
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="cleanXp" value="Excellent" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="cleanXp"
+                    value="Excellent"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Excellent</span>
@@ -200,7 +293,12 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="cleanXp" value="Good" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="cleanXp"
+                    value="Good"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Good</span>
@@ -208,7 +306,12 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="cleanXp" value="Fair" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="cleanXp"
+                    value="Fair"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Fair</span>
@@ -216,20 +319,32 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="cleanXp" value="Bad" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="cleanXp"
+                    value="Bad"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Bad</span>
               </div>
             </div>
+            
+            <div className="radio-error">{errors.cleanXp}</div>
           </div>
 
           <div className="radio-field">
-            <h4>4. Please rate your overall dining experience</h4>
+            <h4>4 . Please rate your overall dining experience.</h4>
             <div className="buttons">
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="diningXp" value="Excellent" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="diningXp"
+                    value="Excellent"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Excellent</span>
@@ -237,7 +352,12 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="diningXp" value="Good" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="diningXp"
+                    value="Good"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Good</span>
@@ -245,7 +365,12 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="diningXp" value="Fair" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="diningXp"
+                    value="Fair"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Fair</span>
@@ -253,16 +378,22 @@ const Form = () => {
 
               <div className="button">
                 <label className="form-radio">
-                  <input type="radio" name="diningXp" value="Bad" onChange={(e) => handleChange(e)}/>
+                  <input
+                    type="radio"
+                    name="diningXp"
+                    value="Bad"
+                    onChange={(e) => handleChange(e)}
+                  />
                   <span className="radio-mark"></span>
                 </label>
                 <span>Bad</span>
               </div>
             </div>
+            <div className="radio-error">{errors.diningXp}</div>
           </div>
-          <div className="submit-btn">
-            <button type="submit">Submit</button>
-          </div>
+          <button type="submit" className="submit-btn">
+            SUBMIT
+          </button>
         </div>
       </form>
     </div>
